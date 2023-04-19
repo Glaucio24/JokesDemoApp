0080f3c0edf31918a1524a78e09760d012c4ce3c
Glaucio Alexandre <108555972+Glaucio24@users.noreply.github.com>
2023-04-19 10:40:21 -0500

    Initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using JokesDemoApp.Models;
+
+namespace JokesDemoApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokesDemoApp.Models.Joke> Joke { get; set; }
+    }
+}
